e154a9bfe2c43ce6e5fa8611133874993ff2d9a6
Oscar Fuentes <ofuentese@gmail.com>
2024-09-30 16:42:40 -0700

    "adding env variables"

--- src/db.js
+++ src/db.js
@@ -1,11 +1,12 @@
 const {Pool} = require('pg')
+const {db} = require('./config')
 
 const pool = new Pool({
-    user: "postgres",
-    password: 'Tengo.Elefante.81',
-    host: 'localhost',
-    port: 5432,
-    database: 'tasksdb'
+    user: db.user,
+    password: db.password,
+    host: db.host,
+    port: db.port,
+    database: db.name
 })
 
 module.exports = pool
