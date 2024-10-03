bad43b6517af6f39f04c255f2b5ebb09ceeb2353
Oscar Fuentes <ofuentese@gmail.com>
2024-10-03 08:59:43 -0700

    delete botton ready

---
+++ client/src/components/Card.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function Card({title, description, id, handleDelete}) {
+  return (
+    <div className="max-w p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2 flex justify-between">
+        <div>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">
+            {title}
+          </h5>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
+        </div>
+        <div>
+          <button 
+            type="button"
+            className="rounded bg-cyan-600 px-6 pb-2 pt-2.5"
+            onClick={()=>console.log("Editing Task")}
+          >
+            Edit
+          </button>
+          <button
+            type="button"
+            className="rounded bg-red-700 px-6 pb-2 ml-2 pt-2.5"
+            onClick={()=>handleDelete(id)}
+            >
+            Delete
+          </button>
+        </div>
+        
+    </div>
+  )
+}
+
+
--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react"
+import Card from './Card';
 
+const URL = "http://localhost:4000/tasks"
 
 export default function TaskList() {
+
+    const [tasks, setTasks] = useState([])
+
+    const loadTasks = async ()=> {
+        const res = await fetch(URL)
+        const data = await res.json()
+        setTasks(data)
+        //console.log(data);
+        
+    }
+
+    const handleDelete = async(id)=>{
+        try {
+            await fetch(`${URL}/${id}`, { 
+                method: "DELETE",
+            })
+            //TODO:validar que el servidor esté arriba
+           setTasks( tasks.filter( task => task.id !== id)   )
+        } catch (error) {
+            console.log(error.message);
+        }
+        
+      }
+   
+    useEffect(()=>{
+        loadTasks()
+    }, [])
   return (
-    <div>TaskList</div>
+    <>
+        <h2>Task List</h2>
+        <div className="grid grid-cols-1">
+            {tasks.map(task => <Card title={task.title} description={task.description} key={task.id} id={task.id} handleDelete={handleDelete}/>)}
+        </div>
+    </>
   )
 }
